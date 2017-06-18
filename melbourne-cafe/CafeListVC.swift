73080ce88da90c263d19e6ce4fcd4abb8e8c3b74
Jing Gao <tracygj1990@gmail.com>
2017-06-18 23:09:49 +1000

    table view start

---
+++ melbourne-cafe/CafeListVC.swift
@@ -0,0 +1,35 @@
+//
+//  CafeListVC.swift
+//  melbourne-cafe
+//
+//  Created by Jing Gao on 18/6/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import UIKit
+
+class CafeListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
